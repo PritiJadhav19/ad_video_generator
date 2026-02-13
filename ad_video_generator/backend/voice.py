285aac70f27364ac34667cffd4a9331267044f25
Priti Jadhav <jpriti330@gmail.com>
2026-02-13 12:51:53 +0530

    Delete ad_video_generator directory

--- ad_video_generator/backend/voice.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import edge_tts
-
-async def synthesize(text: str, out_path: Path, voice: str = "hi-IN-MadhurNeural"):
-    """
-    Async TTS that works inside FastAPI/uvicorn event loop.
-    """
-    communicate = edge_tts.Communicate(text, voice=voice)
-    await communicate.save(str(out_path))
